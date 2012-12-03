f72b3c0f4fa554af3394c0b64e6b8c329b2cb9ef
Milan Felix Sulc <rkfelix@gmail.com>
2012-12-03 13:03:53 +0100

    Typo

--- Priklady/Cesty2.java
+++ Priklady/Cesty2.java
@@ -1,9 +1,6 @@
 /*
  * Priklad: Vytvoření objektu typu File
  */
-
-
-
 package Priklady;
 
 /**
@@ -13,27 +10,27 @@ package Priklady;
 import java.io.*;
 
 class Cesty2 {
+
     public static void main(String[] args) {
-        
+
         File d1 = new File("podslozka");
 
         if (d1.exists() == true) {
             System.out.println("Slozka " + d1.getAbsolutePath() + " existuje.");
-        }
-        else {
+        } else {
             d1.mkdir();
             System.out.println("Slozka '" + d1.getName() + "' byla vytvorena");
         }
 
-    /*
-     * Modifikační metody:
-     * delete(), 
-     * deleteOnExit(),
-     * mkdir(),
-     * mkdirs(),
-     * renameTo(),
-     * setLastModified(),
-     * setReadOnly().
-     */
-  }
+        /*
+         * Modifikační metody:
+         * delete(), 
+         * deleteOnExit(),
+         * mkdir(),
+         * mkdirs(),
+         * renameTo(),
+         * setLastModified(),
+         * setReadOnly().
+         */
+    }
 }
